f65c9f4e44e9c3852693bf550b42031feb5fc890
oliver zulett <oliverzulett@gmail.com>
2022-06-27 22:34:28 -0400

    setup api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix(process.env.API_VERSION);
+  const config = new DocumentBuilder()
+    .setTitle('Image Processor API')
+    .setDescription('Api for image processing')
+    .setVersion(process.env.API_VERSION)
+    .addTag('images')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
