9eac930d6c6085211d94a46514119732ac9ba00a
oliver zulett <oliverzulett@gmail.com>
2022-06-28 22:24:55 -0400

    multer examples

--- src/resources/images/images.controller copy.ts
+++ src/resources/images/images.controller copy.ts
@@ -3,19 +3,17 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
-  UploadedFiles,
 } from '@nestjs/common';
 import { ImagesService } from './images.service';
-import {
-  FileFieldsInterceptor,
-  FileInterceptor,
-} from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
 @Controller('images')
 export class ImagesController {
+  private FILES_PATH = './files';
+
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post('upload-file')
@@ -41,41 +39,5 @@ export class ImagesController {
   storeFile(@UploadedFile() file: Express.Multer.File) {
     console.log(file);
     return file;
-  }
-
-  @Post('store-files')
-  @UseInterceptors(
-    FileFieldsInterceptor(
-      [
-        {
-          name: 'file1',
-          maxCount: 1,
-        },
-        {
-          name: 'file2',
-          maxCount: 1,
-        },
-      ],
-      {
-        storage: diskStorage({
-          destination: './files',
-          filename: (req, file, callback) => {
-            const fileExtName = extname(file.originalname);
-            const fileName = uuidv4();
-            callback(null, `${fileName}${fileExtName}`);
-          },
-        }),
-      },
-    ),
-  )
-  storeFiles(
-    @UploadedFiles()
-    files: {
-      file1?: Express.Multer.File[];
-      file2?: Express.Multer.File[];
-    },
-  ) {
-    console.log(files);
-    return files;
   }
 }
--- src/resources/images/images.module.ts
+++ src/resources/images/images.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    // MulterModule.register({
+    //   dest: './files'
+    // })
+  ],
   controllers: [ImagesController],
   providers: [ImagesService]
 })
